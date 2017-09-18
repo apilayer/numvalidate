4d933efadea1f058228543f5a2dc9e7a459ea239
mmazzarolo <mazzarolomatteo@gmail.com>
2017-09-18 20:06:56 +0200

    Updated links

--- client/pages/Home.js
+++ client/pages/Home.js
@@ -128,13 +128,15 @@ class Home extends Component<Props, void> {
                   Up to {keys.RATE_LIMIT_FOR_PRO_USER_REQUESTS} daily API requests
                 </li>
                 <li className={'Home-plans-plan-description'}>API tokens generation</li>
-                <li className={'Home-plans-plan-description'}>Email support</li>
+                <li className={'Home-plans-plan-description'}>Private email support</li>
               </ul>
             </div>
           </div>
           <p className={'Home-plans-plan-footer'}>
-            Is you project open source? Or maybe you just need an higher rate limit? <br />Read the
-            FAQ below and feel free to <a href={'process.env.EMAIL_ADDRESS'}>contact me</a>
+            If your project is open source or you just need an higher rate limit feel free to{' '}
+            <a href={`mailto:numvalidateapp@gmail.com?subject=I need an higher rate limit`}>
+              contact me
+            </a>
             .
           </p>
         </div>
@@ -150,14 +152,14 @@ class Home extends Component<Props, void> {
               <a href={'https://github.com/googlei18n/libphonenumber'}>Google LibPhoneNumber</a>, a
               phone number formatting and parsing library released by Google, originally developed
               for (and currently used in) Google's Android mobile phone operating system, which uses
-              a several rigorous rules for parsing, formatting, and validating phone numbers for all
+              several rigorous rules for parsing, formatting, and validating phone numbers for all
               countries/regions of the world.
             </p>
           </div>
           <div className={'Home-faq-question'}>
             <h3>What should I use NumValidate for?</h3>
             <p>
-              Validating a phone number allows you to keep your user database clean and fight fraud
+              Validating a phone number allows you to keep your user database clean and fight frauds
               by validating phone numbers at the point of entry into your system. <br /> NumValidate
               also allows you to format a phone number in the{' '}
               <a href={'https://it.wikipedia.org/wiki/E.164'}>E164 format</a>, which is the standard
@@ -168,9 +170,9 @@ class Home extends Component<Props, void> {
             <h3>What's the reason behind the rate limiting?</h3>
             <p>
               Simply put: the rate limit ensures an high quality of service for all API consumers.<br />{' '}
-              To enjoy the default rate limit of {keys.RATE_LIMIT_FOR_FREE_USER_REQUESTS}
-              requests per day, you'll need to sign-up for a free account and then head to the{' '}
-              <a onClick={this._handleDashboardClick}>dashboard</a> to generate an API keys.
+              To enjoy the default rate limit of {keys.RATE_LIMIT_FOR_FREE_USER_REQUESTS} requests
+              per day, you'll need to sign-up for a free account and then head to your dashboard to
+              generate an API keys.
             </p>
           </div>
           <div className={'Home-faq-question'}>
@@ -181,16 +183,19 @@ class Home extends Component<Props, void> {
                 <b>should</b>
               </i>{' '}
               be the main way to pay up the infrastructure.<br /> However, this is an open source
-              project: You can find all its code on Github, and if you think that the proposed price
-              for the pro plan is too high you're free to install it in your own server.<br />
+              project: You can find all its code on{' '}
+              <a href="https://github.com/mmazzarolo/numvalidate-app">Github</a>, and if you think
+              that the proposed price for the pro plan is too high you're free to install it in your
+              own server.<br />
               Payments can be made via Credit Card (Visa, MasterCard, Discover, Diner's Club) and
               are secured by <a href="https://stripe.com">Stripe</a>.<br />You can change your
               payment method at any given time in the Payment section of your Account Dashboard.
             </p>
           </div>
           <div className={'Home-faq-question'}>
-            <h3>Why did you create NumValidate?</h3>
+            <h3>Could you tell us the story behind NumValidate?</h3>
             <p>
+              Of course!<br />
               NumValidate was born in the summer 2017 from a meeting between the holiday boredom and
               the desire to test myself in building a "simple API gateway".<br />You can see by
               yourself in the Github repository that only a small portion of the code handles the
@@ -203,8 +208,8 @@ class Home extends Component<Props, void> {
             <p>
               I'm putting this in the FAQ even if we all know this is not a frequent question by any
               means シ.<br />If you're liking NumValidate, the best way to support the project is by
-              contributing on it on Github: Pull requests with new features/fixes and discussions
-              are warmly welcomed.<br />You can also by subscribing to the pro plan.
+              contributing to it on Github: Pull requests with new features/fixes and discussions
+              are warmly welcomed.<br />
             </p>
           </div>
         </div>
@@ -215,7 +220,10 @@ class Home extends Component<Props, void> {
           <p className={'Home-footer-credit'}>
             Made with ♥ by <a href={'http://mmazzarolo.com'}>Mazzarolo Matteo</a>
           </p>
-          <a href={`mailto:${keys.EMAIL_ADDRESS || ''}?subject=NumValidate`}>Support</a>
+          <div className={'Home-footer-left'}>
+            <a href={`mailto:numvalidateapp@gmail.com?subject=Support`}>Support</a>
+            <a href={`https://github.com/mmazzarolo/numvalidate-app`}>GitHub</a>
+          </div>
         </div>
       </div>
     );
